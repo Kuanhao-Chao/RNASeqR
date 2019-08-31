8dbb3f8eb6cd57a0f7344e045313088754eb1088
HowardChao <ntueeb05howard@gmail.com>
2019-09-01 06:48:39 +1000

    Update Update fastq files

--- R/Update_fastq_gz.R
+++ R/Update_fastq_gz.R
@@ -26,30 +26,48 @@ Update_Fastq_gz <- function(RNASeqRParam,
   fastq.gz.type <- RNASeqRParam@fastq.gz.type
   bool_prepared_fastq_gz <- dir.exists(prepared_fastq_gz)
   if(bool_prepared_fastq_gz) {
-    if (fastq.gz.type == "SE") {
-      # 
-      if (target_samples != "ALL") {
-        
-      } else if (target_samples == "ALL") {
-        
-      }
-    } else if (fastq.gz.type == "PE") {
-      if (target_samples != "ALL") {
-        Checker_result <- TRUE
-        phenoData.result<- phenoDataWrap(path.prefix,
-                                         independent.variable,
-                                         case.group,
-                                         control.group)
-        all.samples <- levels(phenoData.result$pheno_data$ids)
-        for (i in target_samples) {
-          each_checker <- any(i == all.samples)
-          if (!each_checker) {
-            Checker_result <- FALSE
-          }
+    if (target_samples != "ALL") {
+      Checker_result <- TRUE
+      phenoData.result<- phenoDataWrap(path.prefix,
+                                       independent.variable,
+                                       case.group,
+                                       control.group)
+      all.samples <- levels(phenoData.result$pheno_data$ids)
+      for (i in target_samples) {
+        each_checker <- any(i == all.samples)
+        if (!each_checker) {
+          Checker_result <- FALSE
         }
-        if (Checker_result) {
-          # Input samples list is valid. Now check files are inside input_files
-          prepared.files <- list.files(prepared_fastq_gz)
+      }
+      if (!Checker_result) {
+        stop(paste0("target_samples list is invalid. Please check all target ",
+                    "samples are inside 'phenodata.csv'."))
+      }
+      if (Checker_result) {
+        # Input samples list is valid. Now check files are inside input_files
+        prepared.files <- list.files(prepared_fastq_gz)
+        if (fastq.gz.type == "SE") {
+          for (i in target_samples) {
+            message("Checking '", i, "' sample validity ...")
+            each_checker_1 <- any(paste0(i, "_1.fastq.gz") == prepared.files)
+            if (!each_checker_1) {
+              message(paste0("target_samples list is invalid. Please check all target ",
+                             "samples are inside 'phenodata.csv'."))
+              stop(paste0(i, " file does not match. Please check your target", 
+                          " file naming is correct."))
+            }
+          }
+          # Pass every check! Now update files.
+          for (i in target_samples) {
+            message("Moving '", i, "' sample into project ...")
+            unlink(paste0(path.prefix, "gene_data/raw_fastq.gz/", i, "_1.fastq.gz"))
+            paste0(prepared_fastq_gz, '/', i, "_1.fastq.gz")
+            file.symlink(paste0(prepared_fastq_gz, '/', i, "_1.fastq.gz"),
+                         paste0(path.prefix, "gene_data/raw_fastq.gz"))
+            message("    single end", i, " fastq.gz file is updated.")
+          }
+          
+        } else if (fastq.gz.type == "PE") {
           for (i in target_samples) {
             message("Checking '", i, "' sample validity ...")
             each_checker_1 <- any(paste0(i, "_1.fastq.gz") == prepared.files)
@@ -71,40 +89,34 @@ Update_Fastq_gz <- function(RNASeqRParam,
                          paste0(path.prefix, "gene_data/raw_fastq.gz"))
             file.symlink(paste0(prepared_fastq_gz, '/', i, "_2.fastq.gz"),
                          paste0(path.prefix, "gene_data/raw_fastq.gz"))
-            message("    ", i, " fastq.gz files are updated.")
+            message("    paired-end", i, " fastq.gz files are updated.")
           }
-          message("Fastq.gz files are updated!")
-        } else {
-          stop(paste0("target_samples list is invalid. Please check all target ",
-                      "samples are inside 'phenodata.csv'."))
         }
-      } else if (target_samples == "ALL") {
-        message("Checking all samples validity ...")
-        prepared.files <- list.files(prepared_fastq_gz)
-        origin.files <- list.files(paste0(path.prefix, "gene_data/raw_fastq.gz"))
-        if (prepared.files == origin.files) {
-          different_number <- length(setdiff(prepared.files, origin.files))
-          if(different_number == 0) {
-            message("   Samples are valid. Start to update .fastq.gz files")
-            
-            
-            unlink(paste0(path.prefix, "gene_data/raw_fastq.gz/"), recursive = TRUE)
-            
-            dir.create(paste0(path.prefix, "gene_data/raw_fastq.gz/"))
-            raw_fastq.gz.subfiles <- list.files(path = prepared_fastq_gz,
-                                                pattern = sample.pattern,
-                                                recursive = TRUE,
-                                                full.names = TRUE)
-            vapply(raw_fastq.gz.subfiles,
-                   function(x) file.symlink(x, paste0(path.prefix,
-                                                      "gene_data/raw_fastq.gz")),
-                   FUN.VALUE = TRUE)
-            message("All fastq.gz files are updated.")
-          } else {
-            stop("   Samples do not match. Please check your fastq.gz files in",
-                 " 'prepared_fastq_gz':", prepared_fastq_gz)
-          }
-        } 
+        message("(v) Fastq.gz files are updated!! ")
+      }
+    } else if (target_samples == "ALL") {
+      message("Checking all samples validity ...")
+      prepared.files <- list.files(prepared_fastq_gz)
+      origin.files <- list.files(paste0(path.prefix, "gene_data/raw_fastq.gz"))
+      if (prepared.files == origin.files) {
+        different_number <- length(setdiff(prepared.files, origin.files))
+        if(different_number == 0) {
+          message("   Samples are valid. Start to update .fastq.gz files")
+          unlink(paste0(path.prefix, "gene_data/raw_fastq.gz/"), recursive = TRUE)
+          dir.create(paste0(path.prefix, "gene_data/raw_fastq.gz/"))
+          raw_fastq.gz.subfiles <- list.files(path = prepared_fastq_gz,
+                                              pattern = sample.pattern,
+                                              recursive = TRUE,
+                                              full.names = TRUE)
+          vapply(raw_fastq.gz.subfiles,
+                 function(x) file.symlink(x, paste0(path.prefix,
+                                                    "gene_data/raw_fastq.gz")),
+                 FUN.VALUE = TRUE)
+          message("All fastq.gz files are updated.")
+        } else {
+          stop("   Samples do not match. Please check your fastq.gz files in",
+               " 'prepared_fastq_gz':", prepared_fastq_gz)
+        }
       } 
     }
   }
